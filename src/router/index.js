2d9deb9c97560134aecbe93498b69506e4df3ea5
pretty-foam <34409208+pretty-foam@users.noreply.github.com>
2018-02-23 21:16:00 -0600

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import goods from '../components/goods/goods.vue'
-import seller from '../components/seller/seller .vue'
-import ratings from '../components/ratings/ratings.vue'
-import foodDetail from '../components/foodDetail/foodDetail.vue'
+//使用懒加载优化页面加载速度,类似于requireJS加载方式
+const goods=()=>import('../components/goods/goods.vue')
+const seller=()=>import('../components/seller/seller .vue')
+const ratings=()=>import('../components/ratings/ratings.vue')
+const foodDetail=()=>import('../components/foodDetail/foodDetail.vue')
 Vue.use(VueRouter)
-
-
 export default new VueRouter({
    routes:[
      {path:'/',component:goods},

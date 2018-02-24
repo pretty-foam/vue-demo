5fc6d32d26dd6343f3b15b77fdded4d6258238f4
pretty-foam <34409208+pretty-foam@users.noreply.github.com>
2018-02-23 21:12:37 -0600

    Add files via upload

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,23 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import {slide,position} from "../common/js/api";
 Vue.use(Vuex);
 
 const state={
   seller:'vuex seller',
   goods:'vuex goods',
   ratings:'vuex ratings',
-  detailShow:false,
-  starTotal:[],
-  score:0,
-  classMap:['decrease', 'discount', 'special', 'invoice', 'guarantee'],
+  detailShow:false,  //bullentin 详情页面控制开关
+  classMap:['decrease', 'discount', 'special', 'invoice', 'guarantee'],//foods discount className
   price:0,      //total price
   distributionFee:4,
-  count:0,
-  foodTotal:{},  //food detial
-  slideCount:0,
-  ball:[false,false,false,false,false,false,false,false,false],//小球
-  height:0,
+  count:0,  //foods count
+  foodTotal:{},  //foods detial
+  ball:[false,false,false,false,false,false,false,false,false], //balls show controll
+  height:0,  //count button 2D
 };
 
 const getters= {  };
@@ -25,151 +22,57 @@ const getters= {  };
 
 
 const mutations={
-  detailShow(that){
-         that.detailShow=!that.detailShow;
-
-    },
-
-  position($store,e) {
-       let foods=document.querySelector('.foods-wrapper');
-       let name=document.querySelectorAll('.foods-item .foods-title');
-               for(let i of name){
-                 if(i.innerHTML===e){
-                   foods.style.transform=`translateY(${-i.offsetTop}px)`;
-                 }
-               }
-   },
-
-  slide($store,Oj){
-    function touchEvent(el,that,store) {
-      this.el=el;       //目标元素
-      this.that=that;
-      this.store=store;
-      this.Time='';     //时间定时器
-      this.date='';     //获取时间
-      this.currentY=''; //当前的2D的Y偏移值
-      this.startY='';    //鼠标开始Y
-      this.startX='';
-      this.endX='';
-      this.y='';         //开始于结束的2D偏移值差
-      this.i=0 ;          //定时计数
-    }
-    touchEvent.prototype={
-      constructor:touchEvent,
-      start(event){
-        clearInterval(this.Time);
-        this.date=new Date();
-        this.currentY=window.getComputedStyle(this.el).transform.match(/-?\d*/g)[22]||0;
-        this.startY=event.changedTouches[0].screenY;
-        this.startX=event.changedTouches[0].screenX;
-      },
-      move(event){
-        let moveY=event.changedTouches[0].screenY;
-        let length=this.startY-moveY;
-        this.el.style.transform=`translateY(${this.currentY-length-10}px)`
-      },
-      end(event){
-        let length=window.getComputedStyle(this.el).transform.match(/-?\d*/g)[22];
-        this.endX=event.changedTouches[0].screenX;
-        if(length>0){
-          this.el.style.transform='translateY(0)';
-        }else if(length<this.el.parentNode.offsetHeight-this.el.offsetHeight){
-          this.el.style.transform=`translate(0,${this.el.parentNode.offsetHeight-this.el.offsetHeight}px)`
-        }
-        let endY=window.getComputedStyle(this.el).transform.match(/-?\d*/g)[22];
-          this.y= endY-this.currentY;
-        this.date=this.date-new Date;
-        if(this.date>-300&&this.y<-200||this.y>200){
-            this.i=0;
-          this.y>0?this.y=-1:this.y=1;
-          this.Time=setInterval(()=>{this.timeFunction()},60)
-        }
-        this.transverse_sliding();
-      },
-      timeFunction(){
-          let length=parseInt(window.getComputedStyle(this.el).transform.match(/-?\d*/g)[22])-40*this.y;
-          this.el.style.transform=`translateY(${length}px)`;
-          this.i++;
-          if(length>0){
-            this.el.style.transform='translateY(0px)';
-            clearInterval(this.Time);
-          }else if(length<this.el.parentNode.offsetHeight-this.el.offsetHeight||0){
-            this.el.style.transform=`translate(0,${this.el.parentNode.offsetHeight-this.el.offsetHeight}px)`;
-            clearInterval(this.Time);
-          }else if(this.i===10){
-            clearInterval(this.Time);
-          }
-      },
-      transverse_sliding(){
-        let length=this.startX-this.endX;
-        let  routes=['/goods','/ratings','/seller'];
-        if(length>150) {
-           switch(this.store.slideCount){
-             case 1:
-               this.that.$router.push({path:routes[--this.store.slideCount]})
-               ;break;
-             case 2:
-               this.that.$router.push({path:routes[--this.store.slideCount]})
-               ;break;
-           }
-        }
-        else if(length<-150){
-            switch (this.store.slideCount){
-              case 0:
-                this.that.$router.push({path:routes[++this.store.slideCount]})
-                ;break;
-              case 1:
-                 this.that.$router.push({path:routes[++this.store.slideCount]})
-                ;break;
-            }
-        }
+ //小球控制
+  balls($store,e){
+    $store.height=e.path[0].getBoundingClientRect().top-60;
+    for(let i=0 ;i<$store.ball.length;i++){
+      if (!$store.ball[i]){
+        $store.ball[i]=true;
+        return;
       }
     }
-    const p=new touchEvent(Oj.el,Oj.that,$store);
-    Oj.el.addEventListener('touchstart',()=>{p.start(event)},true);
-    Oj.el.addEventListener('touchmove',()=>{p.move(event)},true);
-    Oj.el.addEventListener('touchend',()=>{p.end(event)},true);
-  }
+  },
+ //商品选入数据
+  total($store,data) {
+    const name=data.name;  //food name
+    const judge=data.judge;
+    const that=data.that;   //vue
+    const price=data.price;
+    let foodTotal = $store.foodTotal;  //食品总
+    const foodContent={number:1,price:price};  //初始化食物数据
 
-
-
-
+    if(judge){
+      //!foodTotal[name]? context.state.foodTotal = Object.assign({}, foodTotal,foodTotal[name]=foodContent):foodTotal[name].number++;
+      !foodTotal[name]?that.$set(foodTotal,name,foodContent):foodTotal[name].number++    //js函数本身功能限制监听不到对象数据变化,需使用set
+    }
+    else{
+      //foodTotal[name].number>1?foodTotal[name].number--:that.$set(foodTotal,name,null);
+      foodTotal[name].number>1?foodTotal[name].number--:$store.foodTotal=Object.assign({},foodTotal,foodTotal[name]=null)
+    }
+  },
 
 
 };
 const actions={
-      showClass(context,score){
-        context.commit('detailShow');
-      },
-
-  total(context,data) {
-    const name=data.name;
-    const judge=data.judge;
-    const that=data.that;
-    const price=data.price;
-    let foodTotal = context.state.foodTotal;
-    const foodContent={number:1,price:price};
-    if(judge){
-      //!foodTotal[name]? context.state.foodTotal = Object.assign({}, foodTotal,foodTotal[name]=foodContent):foodTotal[name].number++;
-         !foodTotal[name]?that.$set(foodTotal,name,foodContent):foodTotal[name].number++
-    }
-    else{
-       //foodTotal[name].number>1?foodTotal[name].number--:that.$set(foodTotal,name,null);
-         foodTotal[name].number>1?foodTotal[name].number--:context.state.foodTotal=Object.assign({},foodTotal,foodTotal[name]=null)
-    }
-  },
-  balls(context,e){
-    context.state.height=e.path[0].getBoundingClientRect().y-60;
-    for(let i=0 ;i<context.state.ball.length;i++){
-      if (!context.state.ball[i]){
-          context.state.ball[i]=true;
-          return;
+  send(context,that){
+    that.$http.get('/api/seller').then((res)=>{
+      if(res.data.errno===0){
+        state.seller =res.data.data
       }
-    }
+    });
+    that.$http.get('/api/goods').then((res)=>{
+      res.data.errno===0?state.goods=res.data.data:console.log('error')
+    });
+    that.$http.get('api/ratings').then((res)=>{
+      res.data.errno===0?state.ratings=res.data.data:console.log('error')
+    })
+  },
+  slide(context,data){
+     slide(data)
+  },
+  position(context,e){
+    position(e)
   }
-
-
-
 };
 export default new Vuex.Store({
    state,
